ca90cb2643f810ccbed5145440efd50712332b85
alonecandies <dragonfrezze@gmail.com>
2022-05-24 18:15:38 +0700

    verccel

---
+++ server.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as soup
+from flask import Flask
+import json
+app = Flask(__name__)
+
+
+@app.route('/getTopHolders/<tokenName>')
+def getTopHolder(tokenName):
+    d = webdriver.Chrome()
+    d.get('https://coinmarketcap.com/currencies/'+tokenName+'/holders/')
+    holders = d.find_elements_by_css_selector("table tbody tr")
+    topHolders = []
+    for holder in holders:
+        rank = holder.find_element_by_css_selector("td:nth-child(1)").text
+        address = holder.find_element_by_css_selector("td:nth-child(2)").text
+        amount = holder.find_element_by_css_selector("td:nth-child(3)").text
+        percentage = holder.find_element_by_css_selector(
+            "td:nth-child(4)").text
+        topHolders.append({"rank": rank, "address": address,
+                          "amount": amount, "percentage": percentage})
+    return json.dumps(topHolders, separators=(',', ':'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)
